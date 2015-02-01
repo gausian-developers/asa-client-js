7de30428b32460af18644adf4613d05bfc78f158
yubin <yubin@yahoo-inc.com>
2015-02-01 00:45:41 -0800

    add error parsing

--- asa-client.js
+++ asa-client.js
@@ -29,13 +29,24 @@
   * @param token: string, obtain from asa.gausian.com/authenticate when you sign up your app
   */
   ASA.request = function (string, callback) {
+    if (!string) { // Error: empty
+      callback(null, {errorCode: 1, errorMessage: 'empty request message'});
+    }
+
+    var array = string.split('@');
+    var length = array.length;
+
+    if (length !== 2) { // Error: wrong format
+      callback(null, {errorCode: 2, errorMessage: 'format must be like: <query string>@<app name>'});
+    }
+
     $.ajax({
       url: 'http://asa.gausian.com',
       type: 'POST',
       data: {
         'user_app_id': ASA.appId,
-        'service_app_name': 'CustomerDataReadWrite',
-        'request_string': 'get:all'
+        'service_app_name': array[1],
+        'request_string': array[0]
       }
     }).done(function (data) {
       console.log(data);
